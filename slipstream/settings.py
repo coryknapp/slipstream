b025b91eb7aeaf3ac1e8c0b2bf0df9bb4033e351
Cory Knapp <knapp.cory@gmail.com>
2017-03-16 22:21:21 -0700

    fixed where i didn't set some shit up correctly

--- slipstream/settings.py
+++ slipstream/settings.py
@@ -72,17 +72,15 @@ WSGI_APPLICATION = 'slipstream.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-if not DATABASES:
-    DATABASES = {}
 DATABASES['default'].update(db_from_env)
 
 # Password validation
