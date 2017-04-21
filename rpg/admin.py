58d4a8e0cdf5b3e594251861e6c4c29a75beae59
Cory Knapp <knapp.cory@gmail.com>
2017-04-20 20:45:18 -0700

    added effect to admin panel

--- rpg/admin.py
+++ rpg/admin.py
@@ -4,6 +4,7 @@ from models import RuleSet
 from models import Statistic
 from models import Character
 from models import Campaign
+from models import Effect
 from models import Class
 from models import ClassCollection
 
@@ -15,4 +16,4 @@ admin.site.register(Character)
 admin.site.register(Campaign)
 admin.site.register(Class)
 admin.site.register(ClassCollection)
-
+admin.site.register(Effect)
